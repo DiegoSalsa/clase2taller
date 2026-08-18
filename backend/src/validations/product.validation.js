235b41f9dd63e362dc00e04e4e49fcaa7af971dd
DiegoSalsa <164436357+DiegoSalsa@users.noreply.github.com>
2026-08-18 14:20:23 -0400

    FIX corrige validaciones de productos

--- backend/src/validations/product.validation.js
+++ backend/src/validations/product.validation.js
@@ -1,20 +1,20 @@
 "use strict";
 import Joi from "joi";
 
-export const productQueryValidation = Joi.objetc({
-    id: Joi.number().integer().positive().required(),
+export const productQueryValidation = Joi.object({
+  id: Joi.number().integer().positive().required(),
 }).unknown(false);
 
-export const productCreateValidation = Joi.objetc({
-    nombre: Joi.string().min(2).max(100).required(),
-    precio: Joi.number().integer().min(0).required(),
-    stock: Joi.number().integer().min(0).required(),
+export const productCreateValidation = Joi.object({
+  nombre: Joi.string().min(2).max(100).required(),
+  precio: Joi.number().integer().min(0).required(),
+  stock: Joi.number().integer().min(0).required(),
 }).unknown(false);
 
-export const productUpdateValidation = Joi.objetc({
-    nombre: Joi.string().min(2).max(100),
-    precio: Joi.number().integer().min(0),
-    stock: Joi.number().integer().min(0),
+export const productUpdateValidation = Joi.object({
+  nombre: Joi.string().min(2).max(100),
+  precio: Joi.number().integer().min(0),
+  stock: Joi.number().integer().min(0),
 })
-.or ("nombre", "precio", "stock")
-.unknown(false);
+  .or("nombre", "precio", "stock")
+  .unknown(false);
